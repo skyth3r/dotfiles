2cf13ac084eb91e1327da85d85a654a7c369f2e1
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-23 00:05:00 +0000

    chore: initial commit

---
+++ brew.sh
@@ -0,0 +1,54 @@
+apps=(
+    1password
+    arc
+    arq
+    balenaetcher
+    bartender
+    cryptomator
+    displaperture
+    dropbox
+    discord
+    firefox
+    figma
+    google-chrome
+    github
+    gpg-suite-no-mail
+    iterm2
+    keka
+    keybase
+    menubar-stats
+    notion
+    osxfuse
+    pdf-expert
+    postman
+    quitter
+    raycast
+    rocket
+    slack
+    signal
+    sketch
+    slack
+    spotify
+    the-unarchiver
+    unity-hub
+    veracrypt
+    vlc
+    visual-studio-code   
+    whatsapp
+)
+
+cli=(
+    gh
+    git
+    go
+    hugo
+    mackup
+    node
+    python
+)
+
+# Install cli apps
+brew install "${cli[@]}"
+
+# Install cask packages
+brew --cask install "${apps[@]}"
